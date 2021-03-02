c8cd7886d8a8939c48fffea21d9e3b4854edf0ea
Anil Kumar Yogi <cannotfindanotherme@gmail.com>
2021-03-03 03:07:43 +0530

    spotrf_tile,strsm_tile,load_full

--- left_looking.cu
+++ left_looking.cu
@@ -2,6 +2,7 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>       // needed for the function sqrtf()
 
 #define TILE_SIZE 32 // NB // Block SIZE
 
@@ -77,4 +78,82 @@ __device__ void store_lower(const float* s_data, float* g_data)
         g_data[row * TILE_SIZE + column] = 0;
 
     __syncthreads();
+}
+
+
+/*#include <math.h>       // needed for the function sqrtf()
+# define tile_width 32*/
+
+/*
+* Function to perform Choleshky Factorization for a tile
+*/
+__device__ void spotrf_tile(float* t_A)
+{
+    int ty = blockIdx.x*blockdim.x + threadIdx.x;  // col
+    int tx = blockIdx.y*blockdim.y + threadIdx.y; // row
+
+    for(int k{0};k<tile_width;k++){
+        // square root of diagonal elements
+
+        if(tx==0 && ty==0)
+            t_A[k*tile_width + k] = sqrtf(t_A[k*tile_width + k]);
+        __syncthreads();
+
+        // division step done parallaly
+        if(ty<=tx && tx<tile_width && ty<tile_width)
+        {
+            t_A[(tx+1)*tile_width + k]/= t_A[k*tile_width + k];
+        }
+        __syncthreads();
+
+        if(ty<=tx && tx<tile_width && ty<tile_width){
+            t_A[(tx+1)*tile_width + (ty+1)]-= t_A[(tx+1)*tile_width + k]*t_A[(ty+1)*tile_width + k];
+        }
+        __syncthreads();
+    }
+}
+
+/*
+* Function to perform triangular solve for a tile 
+*/
+
+__device__ void strsm_tile(float *t_A1, float *t_A2)
+{
+    // t_A2 is current unkonown 
+    int ty = blockIdx.x*blockdim.x + threadIdx.x;
+    int tx = blockIdx.y*blockdim.y + threadIdx.y;
+
+    // use syncthreads to remove both top loops
+    for(int m{0};m<tile_width;m++){
+        for(int k{0};k<tile_width;k++){
+            if(tx==0 && ty==0)
+                t_A2[m*tile_width +k]/= t_A1[k*tile_width+k];
+            __syncthreads();
+    
+            if(ty<=tx && tx<tile_width && ty<tile_width)
+            {
+                t_A2[m*tile_width + tx+1+k]-= t_A2[m*tile_width + k]*t_A1[(tx+1+k)*tile_width + k];
+                __syncthreads();
+            }
+
+        }
+    }
+ 
+}
+
+/*
+* Function to load a full tile from global memory to shared memory
+*/
+
+__device__ void load_full(float *t_A,float * S_A)
+{
+    // assigning a 2-D array in shared memory 
+
+    int ty = blockIdx.x*blockdim.x + threadIdx.x;  // col
+    int tx = blockIdx.y*blockdim.y + threadIdx.y; // row
+
+    if(tx<tile_width && ty<tile_width)
+        S_A[tx*tile_width+ty] = t_A[tx*tile_width + ty];
+    __syncthreads();
+
 }
