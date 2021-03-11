41920c64395b04ded9fbf3f15aa5d7c7bcb4595e
Anil Kumar Yogi <cannotfindanotherme@gmail.com>
2021-03-12 01:12:13 +0530

    Corrected few access expressions

--- left_looking.cu
+++ left_looking.cu
@@ -4,17 +4,19 @@
 #include <stdlib.h>
 #include <math.h>       // needed for the function sqrtf()
 
-#define TILE_SIZE 32 // NB // Block SIZE
+#define TILE_SIZE 64 // NB // Block SIZE
 
 /*
  * Function to perform rank-k update 
  * half of the threads working
  */
-
 __device__ void ssyrk_tile(float* rA1, float* rA2) 
 {
-    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
-    int column = blockIdx.x * TILE_SIZE + threadIdx.x;
+    // int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    // int column = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    int row = threadIdx.y;
+    int column = threadIdx.x;
 
     if(column <= row)
     {
@@ -29,32 +31,36 @@ __device__ void ssyrk_tile(float* rA1, float* rA2)
     }
 }
 
+
+
 /*
  * Function to perform general matrix multiplication 
  * DOUBT: I think calculation is given wrong in paper it should be rA2[k][n] 
  */
-
 __device__ void sgemm_tile(const float* rA1, const float* rA2, float* rA3)
 {
-    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
-    int column = blockIdx.x * TILE_SIZE + threadIdx.x;
+    // int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    // int column = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    int row = threadIdx.y;
+    int column = threadIdx.x;
 
 
     float updatedValue = rA3[row * TILE_SIZE + column];
 
     for(int i=0; i<TILE_SIZE; i++)
     {
-        updatedValue -= rA1[row * TILE_SIZE + i] * rA2[column * TILE_SIZE + i];
+        updatedValue -= rA1[row * TILE_SIZE + i] * rA2[i * TILE_SIZE + column];
     }
 
     rA3[row * TILE_SIZE + column] = updatedValue;
 }
 
+
 /*
  * Function to store full tile from shared memory to global memory  
  */
- 
-__device__ void store_full(const float* s_data, float* g_data)
+ __device__ void store_full(const float* s_data, float* g_data)
  {
      int g_row = blockIdx.y * TILE_SIZE + threadIdx.y;
      int g_column = blockIdx.x * TILE_SIZE + threadIdx.x;
@@ -67,11 +73,11 @@ __device__ void store_full(const float* s_data, float* g_data)
      __syncthreads();
  }
 
+
 /*
  * Function to store lower triangular tile from shared memory to global memory  
  */
- 
-__device__ void store_lower(const float* s_data, float* g_data)
+ __device__ void store_lower(const float* s_data, float* g_data)
  {
      int g_row = blockIdx.y * TILE_SIZE + threadIdx.y;
      int g_column = blockIdx.x * TILE_SIZE + threadIdx.x;
@@ -87,14 +93,18 @@ __device__ void store_lower(const float* s_data, float* g_data)
      __syncthreads();
  }
 
+
+
 /*
  * Function to perform Choleshky Factorization for a tile
  */
- 
  __device__ void spotrf_tile(float* t_A)
  {
-     int ty = blockIdx.x*blockDim.x + threadIdx.x;  // col
-     int tx = blockIdx.y*blockDim.y + threadIdx.y; // row
+    //  int ty = blockIdx.x*blockDim.x + threadIdx.x;  // col
+    //  int tx = blockIdx.y*blockDim.y + threadIdx.y; // row
+
+    int ty = threadIdx.x;  // col
+     int tx = threadIdx.y; // row
  
      for(int k{0};k<TILE_SIZE;k++){
          // square root of diagonal elements
@@ -122,12 +132,14 @@ __device__ void store_lower(const float* s_data, float* g_data)
 * Function to perform triangular solve for a tile 
 */
 
-
 __device__ void strsm_tile(float *t_A1, float *t_A2)
 {
 	// t_A2 is current unkonown 
-	int ty = blockIdx.x*TILE_SIZE + threadIdx.x;
-	int tx = blockIdx.y*TILE_SIZE + threadIdx.y;
+	// int ty = blockIdx.x*TILE_SIZE + threadIdx.x;
+	// int tx = blockIdx.y*TILE_SIZE + threadIdx.y;
+
+    int ty = threadIdx.x;
+	int tx = threadIdx.y;
 	
 	for(int i{0};i<TILE_SIZE;i++){
 		if(ty==0){
@@ -163,4 +175,3 @@ __device__ void load_full(float *t_A,float * S_A)
     __syncthreads();
 
 }
-
